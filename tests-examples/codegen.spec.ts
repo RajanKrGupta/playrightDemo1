07f5bdbacb792493fd3ea5f7219fe73f9fe7a82b
aadyadvik <raajan.kr.gupta@gmail.com>
2024-11-25 20:40:50 +0530

    playwright first push'

---
+++ tests-examples/codegen.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('Adding product SauceLab', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="item-4-title-link"]').click();
+  await page.locator('[data-test="remove"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+  expect(page.getByText('Swag Labs')).toBeVisible();
+
+});
+
---
+++ tests-examples/hook_example.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+//2 tcs grouped under Nabigation name
+
+test.describe('navigation', () => 
+  {
+  
+    test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto('https://playwright.dev/');
+  });
+
+  
+  test('main navigation', async ({ page }) => {
+    // Assertions use the expect API.
+    await expect(page).toHaveURL('https://playwright.dev/');
+  });
+
+  test('get started link', async ({ page }) => {
+
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+  
+    // Expects page to have a heading with the name of Installation.
+    await expect(page).toHaveURL('https://playwright.dev/docs/intro');
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
+
+   
+  test.afterEach(async ({page}) => {
+    // Go to the starting url before each test.
+    await page.close();
+  });
+
+
+});
